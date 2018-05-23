9374f03a28f2b819c88165a13d6c89dc3f1eb0be
DESKTOP-T9TKHKU\Zhenya <ekovtunov13@gmail.com>
2018-05-23 14:19:14 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { HeaderPageComponent } from './header-page/header-page.component';
+import { MainSliderComponent } from './main-slider/main-slider.component';
+import { MostPopularProductComponent } from './most-popular-product/most-popular-product.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPageComponent,
+    MainSliderComponent,
+    MostPopularProductComponent,
+    TestimonialsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
---
+++ src/app/most-popular-product/most-popular-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-most-popular-product',
+  templateUrl: './most-popular-product.component.html',
+  styleUrls: ['./most-popular-product.component.scss']
+})
+export class MostPopularProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
