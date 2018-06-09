505813dccac4e8d2c5f004999d68f000c3ef61e5
DESKTOP-T9TKHKU\Zhenya <ekovtunov13@gmail.com>
2018-06-09 19:50:15 +0300

    Test Slider

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-declare var jQuery: any;
 declare var test: any;
-declare var $: any;
-import * as $ from "jquery";
+
+
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
@@ -10,10 +9,23 @@ import * as $ from "jquery";
 })
 export class TestimonialsComponent implements OnInit {
 
+
+  ngAfterViewInit(): void {
+    this.loadScript('assets/js/slider.js');
+  }
+
+  public loadScript(url) {
+    console.log('preparing to load...')
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
+ }
+
   constructor(private _elementRef: ElementRef) { }
 
   ngOnInit() {
-    $(document).ready(function(){
+   /* $(document).ready(function(){
       var carousel=$(".carousel").waterwheelCarousel({
         flankingItems:3
       });
@@ -26,14 +38,14 @@ export class TestimonialsComponent implements OnInit {
         return false;
       });
       
-    });
+    });*/
   }
 
-  ngAfterViewInit() {
+ /* ngAfterViewInit() {
     var jquery = document.createElement("script");
     jquery.type = "text/javascript";
     jquery.src = "../assets/js/jquery.waterwheelCarousel.min.js";
     this._elementRef.nativeElement.appendChild(jquery);
-  }
+  }*/
 
 }
